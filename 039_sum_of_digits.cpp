9e5ff2bdc89115a3dc0eb5a4b3a35c1c74ab13f6
Witold Formański <115305048+witek-formanski@users.noreply.github.com>
2022-11-28 14:32:38 +0100

    Add files via upload

---
+++ 039_sum_of_digits.cpp
@@ -0,0 +1,32 @@
+﻿//  2022-11-28
+//      14:16 - 14:31
+
+#include <iostream>
+// #include <typeinfo>
+using namespace std;
+
+
+int sum_of_digits(int example_int) {    
+    auto example_string = to_string(example_int);
+    // cout << typeid(example).name() << endl;
+    // cout << typeid(example_string).name() << endl;
+    // cout << typeid(example_string[0]).name() << endl;
+    int ans;
+    for (int i = 0; i < example_string.length(); i++) {
+        int digit = example_string[i] - '0';
+        ans += digit;
+    }
+    return ans;
+    
+}
+
+
+int example = 13963;
+
+int main() {
+    cout << sum_of_digits(example);
+}
+
+
+
+
